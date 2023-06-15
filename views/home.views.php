7fc245ad017094e538a03b0eda4e58d4a087029f
Narayan Dhakal <narayandhakal443@gmail.com>
2023-06-15 19:31:14 +0545

    chore: Home view file
    contains basic structure skeleton

---
+++ views/home.views.php
@@ -0,0 +1,49 @@
+<!-- header -->
+<?php
+include "views/header.php";
+// body code here
+// echo hostname();
+
+?>
+
+<!-- header end-->
+
+<!-- body -->
+
+<!-- <img src="< ?= HOSTNAME ?>/assets/images/gg.webp" alt="" srcset=""> -->
+<?php
+// $query =  "Select * from site_info";
+// use Model;
+
+
+
+// $user = new Model;
+// // change variable data of class model
+// $user->table = "site_info";
+// $result = $user->one(); 
+
+// show($result);
+
+
+
+?>
+
+ 
+
+
+
+<ul>
+    <li>navbar</li>
+    <li>carausol</li>
+    <li>event table</li>
+    <li>event tablefaqs</li>
+
+</ul>
+
+
+
+<!-- body end -->
+
+<!-- footer -->
+<?php include "views/footer.php"; ?>
+<!-- footer end -->
