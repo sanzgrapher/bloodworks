724afca0c8ea98e9fea3eb90a2db17ea81e9c3ed
Narayan Dhakal <narayandhakal443@gmail.com>
2023-06-15 19:17:47 +0545

    feat(function): Just a echo function show(value)
    prints inside pre tag

---
+++ includes/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function show($stuff)
+{
+    // printing anything sent as parameter
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+$hostname = "http://localhost/bloodworks/";
+
