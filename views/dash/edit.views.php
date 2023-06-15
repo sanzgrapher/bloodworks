74fb5d06089936128d39ccb0ac552093c4d36f9d
Narayan Dhakal <narayandhakal443@gmail.com>
2023-06-15 21:22:33 +0545

    feat(Edit views): Edit users data from dashboard

--- views/dash/edit.views.php
+++ views/dash/edit.views.php
@@ -1,1 +1,37 @@
-edit ayo
+<?php 
+//get user
+$user = new Model();
+$user->table = "user";
+$data = [
+    "id" => "6"
+];
+$data_not=['fname'];
+$result = $user->where($data );
+show($result);
+
+// print $result['fname'];
+
+
+if(isset($_POST['submit'])){
+    $user->table = "user";
+    $data = [
+        "id" => $_POST['id'], 
+        "lname" => $_POST['email']
+    ];
+    $user->update(6,$data);
+    redirect("edit");
+}
+
+
+?>
+
+<form action="edit" method="post">
+ <?php foreach($result as $row){ ?>
+    <input type="hidden" name="id" value="<?= $row->id; ?>">
+    <input type="text" name="name" value="<?= $row->fname; ?>">
+    <input type="text" name="email" value="<?= $row->lname; ?>">
+  
+    <input type="submit" name="submit" value="submit">
+<?php }?>
+
+</form>
