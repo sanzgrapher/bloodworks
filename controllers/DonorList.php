0f06c8eb71cfa5c371b13331ce63ca65116de7c2
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-05 20:18:41 +0545

    refactor: donor list page
    listing donors info in simple table

--- controllers/DonorList.php
+++ controllers/DonorList.php
@@ -10,6 +10,29 @@ class Donorlist
 
 
 
-        $this->view('donor-list');
+        
+        $donor = $this->donor();  
+        $data = [
+             
+            "donors" => $donor
+        ];
+
+        $this->view('donor-list', $data); // Pass the $bloodbanklist to the view
+    }
+    private function donor()
+    {
+        $donorlist = new Model;
+        $donorlist->table = "user";
+        $data = [
+                "donor_availability" => "Available"
+            ];
+        $donor_list = $donorlist->where($data);
+
+
+
+
+        return $donor_list;
+        // Do something with the $bloodbanklist data
+        // For example, you can pass it to the view or perform further operations
     }
 }
