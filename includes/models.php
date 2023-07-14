1c5c78f51357c9b2d77925018a6ea343e7f62cfe
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-14 08:23:59 +0545

    feat(controllers): bloodbank controller
    blood bank controller and basic function

--- includes/models.php
+++ includes/models.php
@@ -15,7 +15,7 @@ class Model
     protected $limit = '10';
     protected $offset = 0;
     protected $order_type = "desc";
-    protected $order_column = "id";
+    public $order_column = "id";
 
     public function findAll()
     {
@@ -42,7 +42,7 @@ class Model
         $query = trim($query, " && ");
 
         $query .= " order by $this->order_column $this->order_type limit $this->limit offset $this->offset";
-        //    echo $query;
+           echo $query;
         $data = array_merge($data, $data_not);
         return $this->query($query, $data);
     }
@@ -84,3 +84,67 @@ class Model
         }
         return false;
     }
+    public function insert($data)
+    {
+        // remove unwanted or not allowed data
+        if (!empty($this->allowedColumns)) {
+            foreach ($data as $key => $value) {
+                if (!in_array($key, $this->allowedColumns)) {
+                    unset($data[$key]);
+                }
+            }
+        }
+
+        $keys = array_keys($data);
+
+
+        $query = "insert into $this->table (" . implode(",", $keys) . ") values (:" . implode(" , :", $keys) . ")";
+
+        $this->query($query, $data);
+        return false;
+    }
+    public function update($id, $data, $id_column = 'id')
+    {
+
+        // remove unwanted or not allowed data
+        if (!empty($this->allowedColumns)) {
+            foreach ($data as $key => $value) {
+                if (!in_array($key, $this->allowedColumns)) {
+                    unset($data[$key]);
+                }
+            }
+        }
+
+        $keys = array_keys($data);
+
+        $query = "update $this->table set ";
+
+        foreach ($keys as $key) {
+            $query .= $key . " = :" . $key . ", ";
+        }
+
+
+
+
+        $query = trim($query, ", ");
+
+        $query .= " where $id_column = :$id_column ";
+        // $data = [$id_column => $id];
+        $data[$id_column] = $id;
+        // echo $query;
+        $this->query($query, $data);
+        return false;
+    }
+
+    public function delete($id, $id_column = 'id')
+    {
+
+        $data[$id_column] = $id;
+        $query = "delete from $this->table where $id_column = :$id_column";
+
+        //    echo $query;
+        $this->query($query, $data);
+
+        return false;
+    }
+}
