24a6da7ca7697f919a78c4884981039eda5d7d4e
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-15 09:30:27 +0545

    refactor: views components
    edited views components

--- views/blood-banks.views.php
+++ views/blood-banks.views.php
@@ -5,37 +5,38 @@
     <div class="table-section">
         <h2 class="table-sec-title fw-bold display-5">Available Bloodbanks</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi harum accusantium ducimus vero ipsa dolore sint praesentium veritatis hic sapiente, nemo vitae perferendis quis inventore numquam quo totam reiciendis?</p>
+        <div style="overflow-x:auto;">
+            <table id="datatable" class="table table-hover table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th class="th-sm">Name
+                        </th>
+                        <th class="th-sm">Position
+                        </th>
 
-        <table id="datatable" class="table table-hover table-striped table-bordered table-sm" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th class="th-sm">Name
-                    </th>
-                    <th class="th-sm">Position
-                    </th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (empty($bloodbanklist)) {
-                    echo "No donor available";
-                } else {
-                    foreach ($bloodbanklist as $bloodbank) { ?>
-                        <tr>
-                            <td><?= $bloodbank->bb_id . " " . $bloodbank->bb_name; ?></td>
-                            <td><?= $bloodbank->bb_email; ?></td>
-                            <!-- <td>Contach Btn</td> -->
-                        </tr>
-                <?php
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if (empty($bloodbanklist)) {
+                        echo "No donor available";
+                    } else {
+                        foreach ($bloodbanklist as $bloodbank) { ?>
+                            <tr>
+                                <td><?= $bloodbank->bb_id . " " . $bloodbank->bb_name; ?></td>
+                                <td><?= $bloodbank->bb_email; ?></td>
+                                <!-- <td>Contach Btn</td> -->
+                            </tr>
+                    <?php
+                        }
                     }
-                }
-                ?>
+                    ?>
 
-            </tbody>
+                </tbody>
 
-        </table>
+            </table>
 
-    </div>
+        </div>
+        </div>
 
 </main>
 <?php require "footer.php"; ?>
--- views/dash/edit.views.php
+++ views/dash/edit.views.php
@@ -1,4 +1,5 @@
 <?php
+
 //get user
 // $user = new Model();
 // $user->table = "user";
@@ -32,14 +33,13 @@ $cookie_name = "status";
 
 
 ?>
+<?php
+include_once 'views/header.php';
 
-<h1>Dash edit</h1>
-<ul>
-    <li><a href="home">Home</a></li>
-    <li><a href="blood-banks">Bloodbanks</a></li>
-    <li><a href="Donor-list">Donor lists</a></li>
-    <li><a href="dash/edit">Donor lists</a></li>
-</ul>
+
+?>
+
+ 
 <?php
 if (!isset($_COOKIE["STATUS"])) {
     // echo "Cookie named '" . $cookie_name . "' is not set!";
@@ -50,61 +50,206 @@ if (!isset($_COOKIE["STATUS"])) {
     echo "SET";
 }
 ?>
-
-<h1>basic </h1>
-<form action="edit" method="post">
-    <?php foreach ($loggedinuser as $user) { ?>
-        <input type="hidden" name="id" value="<?= $user->id; ?>">
-        <input type="text" name="fname" value="<?= $user->fname; ?>">
-        <input type="text" name="email" value="<?= $user->email; ?>">
-
-        <input type="submit" name="submit" value="submit">
-    <?php } ?>
-
-</form>
-
-
-<!-- Basic Details -->
-<h3>basic details</h3>
-<form action="edit" method="post">
-    <label for="fname">First Name</label>
-    <input type="text" name="fname" value="<?= $user->fname; ?>">
-    <label for="lname">Middl Name</label>
-    <input type="text" name="mname" value="<?= $user->mname; ?>">
-    <label for="lname">Last Name</label>
-    <input type="text" name="lname" value="<?= $user->lname; ?>">
-    <label for="email">Email</label>
-    <input type="text" name="email" value="<?= $user->dateofbirth; ?>">
-    <label for="phone">Phone</label>
-    <input type="text" name="phone" value="<?= $user->email; ?> ">
-    <label for="dob">Date of Birth</label>
-    <input type="date" name="dob" value="<?= $user->username; ?> ">
-    <label for="bloodgroup">Bloodgroup</label>
-    <input type="bloodgroup" name="bloodgroup" value="<?= $user->bloodgroup; ?> ">
-    <label for="gender">gender</label>
-    <select name="gender" id="gender">
-        <option value="male">M</option>
-        <option value="female">F</option>
-        <option value="other">other</option>
-    </select>
-
-
-</form>
-
-<h3>medical details</h3>
-<form action="edit" method="post">
-    <label for="bloodgrou">Blood Group </label>
-    <select name="bloodgroup" id="bloodgroup">
-        <option value="A+">A+</option>
-        <option value="B+">B+</option>
-        <option value="AB+">AB+</option>
-        <option value="O+">O+</option>
-        <option value="A-">A-</option>
-        <option value="B-">B-</option>
-        <option value="AB-">AB-</option>
-        <option value="O-">O-</option>
-    </select>
+<main class="dash-container">
+    <section class="dashboard_container">
+        <div class="dashboard ">
+            <div class="profile-container">
+                <img src="img.jpg" alt="" class="profile-img">
+                <a href="#" class="upload-img">Upload Image</a>
+                <h3>Username</h3>
+            </div>
+            <div class="side-list">
+                <a href="#">
+                    <i class="fa-solid fa-chart-simple active"></i>Dashboard</a>
+                <a href="#">
+                    <i class="fa-regular fa-user"></i>My Profile</a>
+                <a href="#">
+                    <i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a>
+                <a href="#">
+                    <i class="fa-regular fa-trash-can"></i>Delete Profile</a>
+            </div>
+            <div class="hamburger1">
+                <div class="bar1"></div>
+                <div class="bar1"></div>
+                <div class="bar1"></div>
+            </div>
+        </div>
+        <div class="info ">
+            <div class="container">
+                <div class="heading">
+                    PROFILE
+                </div>
+                <form action="edit" method="post">
+                    <?php foreach ($loggedinuser as $user) { ?>
+                        <div class="form first">
+                            <div class="details personal">
+                                <span class="title">Personal Details</span>
+                                <input type="hidden" name="id" value="<?= $user->id; ?>">
+                                <div class="fields">
+                                    <div class="input_field">
+                                        <label>First Name</label>
+                                        <input type="text" value="<?= $user->fname; ?> " placeholder="Enter your First name" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Middle Name</label>
+                                        <input type="text" value="<?= $user->mname; ?>" placeholder="Enter your Middle name">
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Last Name</label>
+                                        <input type="text" value="<?= $user->lname; ?>" placeholder="Enter your Last name" required>
+                                    </div>
 
 
-</form>
-<h3>Address</h3>
+                                    <div class="input_field">
+                                        <label>Username</label>
+                                        <input type="text" placeholder="Enter your username" required>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>E-mail</label>
+                                        <input type="text" placeholder="Enter your e-mail" value="<?= $user->email; ?>" required>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Password</label>
+                                        <input type="password" placeholder="Enter your password" required>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Date of Birth</label>
+                                        <input type="date" placeholder="Select Date" required>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Mobile Number</label>
+                                        <input type="number" placeholder="Enter your mobile number" required>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Gender</label>
+                                        <select name="gender">
+                                            <option value="">Select Gender</option>
+                                            <option value="female">Female</option>
+                                            <option value="male">Male</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Blood Group</label>
+                                        <select name="gender">
+                                            <option value="">Select Your Blood Group</option>
+                                            <option value="apositive">A Positive</option>
+                                            <option value="anegative">A Negaitive</option>
+                                            <option value="bpositive">B Positive</option>
+                                            <option value="bnegative">B Negative</option>
+                                            <option value="abpositive">AB Positive</option>
+                                            <option value="abnegative">AB Negative</option>
+                                            <option value="opositive">O Positive</option>
+                                            <option value="onegative">O Negative</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="input_field">
+                                        <label>Address</label>
+                                        <input type="number" placeholder="Enter your address" required>
+                                    </div>
+                                </div>
+                                <button class="submit" type="submit" name="submit" value="submit">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </div> <?php } ?>
+                </form>
+            </div>
+
+
+            <div class="container">
+                <div class="heading">
+                    PROFILE
+                </div>
+                <form action="#">
+                    <div class="form first">
+                        <div class="details personal">
+                            <span class="title">Personal Details</span>
+
+                            <div class="fields">
+                                <div class="input_field">
+                                    <label>Full Name</label>
+                                    <input type="text" placeholder="Enter your name" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Username</label>
+                                    <input type="text" placeholder="Enter your username" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>E-mail</label>
+                                    <input type="text" placeholder="Enter your e-mail" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Password</label>
+                                    <input type="password" placeholder="Enter your password" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Date of Birth</label>
+                                    <input type="date" placeholder="Select Date" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Mobile Number</label>
+                                    <input type="number" placeholder="Enter your mobile number" required>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Gender</label>
+                                    <select name="gender">
+                                        <option value="">Select Gender</option>
+                                        <option value="female">Female</option>
+                                        <option value="male">Male</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Blood Group</label>
+                                    <select name="gender">
+                                        <option value="">Select Your Blood Group</option>
+                                        <option value="apositive">A Positive</option>
+                                        <option value="anegative">A Negaitive</option>
+                                        <option value="bpositive">B Positive</option>
+                                        <option value="bnegative">B Negative</option>
+                                        <option value="abpositive">AB Positive</option>
+                                        <option value="abnegative">AB Negative</option>
+                                        <option value="opositive">O Positive</option>
+                                        <option value="onegative">O Negative</option>
+                                    </select>
+                                </div>
+
+                                <div class="input_field">
+                                    <label>Address</label>
+                                    <input type="number" placeholder="Enter your address" required>
+                                </div>
+                            </div>
+                            <button class="submit" type="submit">
+                                Update Profile
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+
+        </div>
+    </section>
+</main>
+
+
+
+
+ 
+
+
