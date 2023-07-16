623ecc1eeef56e10974a4b80c55acb1c76cf9827
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-16 07:01:46 +0545

    feat(controllers): Add Blood bank
    bloodbank form sends for admin request

--- views/add-bloodbank.views.php
+++ views/add-bloodbank.views.php
@@ -15,7 +15,7 @@ $sn = 0;
         <h1 class="table-sec-title fw-bold display-5">Add Blood Bank</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi harum accusantium ducimus vero ipsa dolore sint praesentium veritatis hic sapiente, nemo vitae perferendis quis inventore numquam quo totam reiciendis?</p>
 
-        <form class="mx-auto "   action="#">
+        <form class="mx-auto " method="POST" action="addbloodbank">
             <div class="form first">
                 <div class="details personal">
 
@@ -27,35 +27,36 @@ $sn = 0;
 
                         <div class="input_field">
                             <label>Blood Bank Name</label>
-                            <input type="text" placeholder="Enter your Blood Bank Name" required>
+                            <input type="text" name="bb_name" placeholder="Enter your Blood Bank Name" required>
                         </div>
                         <div class="input_field">
                             <label>E-mail</label>
-                            <input type="text" placeholder="Enter  e-mail" required>
+                            <input type="text" name="bb_email" placeholder="Enter  e-mail" required>
                         </div>
                         <div class="input_field">
                             <label>Mobile Number</label>
-                            <input type="text" maxlength="10" name="phone_no" placeholder="Enter your mobile number" required>
+                            <input type="text" maxlength="10" name="bb_phoneno" placeholder="Enter your mobile number" required>
+                        </div>
+                        <div class="input_field">
+                            <label>Address</label>
+                            <input type="text" name="bb_address" placeholder=" City-ward no,tole eg,(Pokhara-17,Chhorepatan)" required>
                         </div>
                         <div class="input_field">
                             <label>Description</label>
-                            <textarea name="" id="" cols="10" rows="5"></textarea>
+                            <textarea name="bb_description" id="" placeholder="Enter Description For your Bloodbank" cols="10" rows="5"></textarea>
                         </div>
-
-
-
-
-
-
-
-
-
-
-
+                        <div class="input_field">
+                            <label>Username</label>
+                            <input type="text" name="bb_username" placeholder="Enter your username" required>
+                        </div>
+                        <div class="input_field">
+                            <label>Password</label>
+                            <input type="password" name="bb_password" placeholder="Enter your password" required>
+                        </div>
 
                     </div>
 
-                    <input class="submit button" type="submit" name="edit-login" value="Send For Approval">
+                    <input class="submit button" type="submit" name="add-bloodbank" value="Send For Approval">
                 </div>
             </div>
         </form>
