6f8b700d8b0f51667d901ffad6cc4f23c0bf54d0
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-17 17:20:13 +0545

    feat(controllers): Login Validate Complete

--- controllers/Login.php
+++ controllers/Login.php
@@ -7,12 +7,51 @@ class Login
     public function index($a = "", $b = "", $c = "")
     { // default function should be in every class
 
+        $this->view('login');
+        if (isset($_POST['login'])) {
+            $this->validateLogin();
+        }
+    }
 
-      
+    public function validateLogin()
+    {
 
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
+        $user = new Model;
+        $user->table = "user";
 
-        $this->view('login'); // from controller class 
+        $data = [
+            "username" => $username
+        ];
 
+        $user_return = $user->where($data);
+
+        $userdata = $user_return[0];
+
+    
+       
+
+        
+       
+        if (count($user_return) == 1) {
+
+           
+            // if (password_verify($password, $user->password)) {
+            if ($password == $userdata->password) {
+                
+                $_SESSION['userid'] = $userdata->id;
+               
+redirect(HOSTNAME."dash");  
+               
+        
+            }else{
+                echo "password not match";
+            }
+
+        } else {
+            echo "user not found";
+        }
     }
 }
