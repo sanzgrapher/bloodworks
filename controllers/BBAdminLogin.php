f69f138999b6d59b7966f091a72ac71e765ee5a2
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-18 01:03:44 +0545

    login sys

--- controllers/BBAdminLogin.php
+++ controllers/BBAdminLogin.php
@@ -1,12 +1,66 @@
 <?php
 
+use function PHPSTORM_META\type;
+
 class BBAdmin
 {
     use Controller;
 
-    public function index($a = "", $b = "", $c = "")
-    { // default function should be in every class
-        $this->view('bbadmin/login'); // from controller class 
+    public function index($a = "", $b = "", $c = ""){
 
+        $this->view('bbadmin/login');
+
+        if (isset($_POST['login'])) {
+            $this->validateLogin();
+        }
     }
+
+    public function validateLogin()
+    {
+
+        $username = $_POST['bb_username'];
+        $password = $_POST['bb_password'];
+
+        $user = new Model;
+        $user->table = "blood_banks";
+        $user->order_column = "bb_id";
+
+        $data = [
+            "bb_username" => $username,
+            "bb_status" => "verified"
+         
+
+        ];
+
+        $user_return = $user->where($data);
+    //  check for empty array
+    if (empty($user_return)) {
+
+        die("user not found /  not verified");
+    }
+
+
+        if (count($user_return) == 1) {
+              $userdata = $user_return[0];
+
+  
+           
+            if ($password == $userdata->bb_password  ) {
+
+                $_SESSION['bb_id'] = $userdata->bb_id;
+
+                redirect(HOSTNAME . "bbadmin");
+            } else {
+                echo "password not match";
+                die();
+            }
+        } else {
+            echo "user not found or not verified";
+            die();
+            
+        }
+    }
+
+
+
 }
---
+++ views/bbadmin/login.views.php
@@ -0,0 +1,6 @@
+<form action="login" method="post">
+    <input type="text" name="bb_username" placeholder="username">
+    <input type="password" name="bb_password" placeholder="password">
+    <input type="submit" value="login" name="login">
+
+</form>
