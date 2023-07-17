7a6859ffe6f6ec20f93bb71f152fbf8477d3c2f2
ajaayushdeku <be2020se698@gces.edu.np>
2023-07-17 18:50:17 +0545

    bb-edit file update

--- views/bbadmin/edit.views.php
+++ views/bbadmin/edit.views.php
@@ -9,53 +9,52 @@
  <main class="dash-container">
      <section class="dashboard_container">
          <?php include_once 'asidemenu.php'; ?>
-
-
          <div class="info ">
-             <div class="container">
-                 <div class="heading">
-                     Blood Bank Home admin
-                 </div>
-                 <form action="#">
-                     <div class="form first">
-                         <div class="details personal">
-                             <span class="title">Personal Details</span>
+         <div class="container">
+                <div class="heading">
+                    PROFILE
+                </div>
+                <form action="edit" method="post">
+                    <?php foreach ($loggedinuser as $user) { ?>
+                        <div class="form first">
+                            <div class="details personal">
+                                <span class="title">Personal Details</span>
+                                <input type="hidden" name="bb_id" value="<?= $user->bb_id; ?>">
+                                <div class="fields">
+                                    <div class="input_field">
+                                        <label>Blood Bank Name</label>
+                                        <input type="text" name="bb_name" value="<?= $user->bb_name; ?> " placeholder="Enter your blood bank name" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Username</label>
+                                        <input type="text"  name="bb_username" value="<?= $user->bb_username ?>"placeholder="Enter your username" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Contact Number</label>
+                                        <input type="text" maxlength="10" name="bb_phoneno" value="<?= $user->bb_phoneno ?>" placeholder="Enter the blood bank contact number" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Address</label>
+                                        <input type="text" name="bb_address" value=" <?= $user->bb_address; ?>" placeholder="Enter the blood bank address" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Status</label>
+                                        <input type="text" name="bb_status" value=" <?= $user->bb_status; ?>" placeholder="Enter the status of the bloodbank" required>
+                                    </div>
+                                    <div class="input_field">
+                                        <label>Description</label>
+                                        <input type="text" name="bb_description" value=" <?= $user->bb_descriptionname; ?>" placeholder="Enter the blood bank description" required>
+                                    </div>
+                                </div>
+                                <input class="submit button" type="submit" name="edit-basic" value="Update Details">
+                            </div>
+                        </div> <?php } ?>
+                </form>
+            </div>
 
-                             <div class="fields">
 
 
 
-
-                                 <div class="input_field">
-                                     <label>E-mail</label>
-                                     <input type="text" placeholder="Enter your e-mail" required>
-                                 </div>
-
-                                 <div class="input_field">
-                                     <label>Password</label>
-                                     <input type="password" placeholder="Enter your password" required>
-                                 </div>
-                                 <div class="input_field">
-                                     <label>Password</label>
-                                     <input type="password" placeholder="ReEnter your password" required>
-                                 </div>
-
-
-
-
-
-
-
-
-
-
-                             </div>
-
-                             <input class="submit button" type="submit" name="edit-login" value="Update Login">
-                         </div>
-                     </div>
-                 </form>
-             </div>
 
 
              <div class="container">
@@ -68,33 +67,19 @@
                              <span class="title">Personal Details</span>
 
                              <div class="fields">
-
-
-
-
                                  <div class="input_field">
                                      <label>E-mail</label>
-                                     <input type="text" placeholder="Enter your e-mail" required>
+                                     <input type="text" name="bb_email" placeholder="Enter your e-mail" required>
                                  </div>
 
                                  <div class="input_field">
                                      <label>Password</label>
-                                     <input type="password" placeholder="Enter your password" required>
+                                     <input type="password" name="bb_password" placeholder="Enter your password" required>
                                  </div>
                                  <div class="input_field">
                                      <label>Password</label>
-                                     <input type="password" placeholder="ReEnter your password" required>
+                                     <input type="password" name="bb_password" placeholder="ReEnter your password" required>
                                  </div>
-
-
-
-
-
-
-
-
-
-
                              </div>
 
                              <input class="submit button" type="submit" name="edit-login" value="Update Login">
