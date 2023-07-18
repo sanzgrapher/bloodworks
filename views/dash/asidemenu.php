b08d7cda07132187519aa4ca538e36a507a2f200
Narayan Dhakal <narayandhakal443@gmail.com>
2023-07-18 16:10:28 +0545

    logout

--- views/dash/asidemenu.php
+++ views/dash/asidemenu.php
@@ -1,30 +1,27 @@
- 
-    <div class="dashboard ">
-         <?php  foreach ($loggedinuser as $user) { 
-                ?>  
-        <div class="profile-container">
-            <!-- <img src="img.jpg" alt="" class="profile-img"> -->
-            <!-- <a href="#" class="upload-img">Upload Image</a> -->
-            <h3><?= $user->username ?></h3>
-        </div>
-        <div class="side-list">
-            <a href="<?= HOSTNAME ?>dash/">
-                <i class="fa-solid fa-chart-simple active"></i>Home</a>
-            <a href="<?= HOSTNAME ?>dash/requestlist.views.php">
-                <i class="fa-solid fa-user"></i>Request List</a>
-            <a href="<?= HOSTNAME ?>dash/edit">
-                <i class="fa-regular fa-user"></i>Settings</a> 
-            <a href="#">
-                <i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a>
+ <div class="dashboard ">
+     <?php foreach ($loggedinuser as $user) {
+        ?>
+         <div class="profile-container">
+             <!-- <img src="img.jpg" alt="" class="profile-img"> -->
+             <!-- <a href="#" class="upload-img">Upload Image</a> -->
+             <h3><?= $user->username ?></h3>
+         </div>
+         <div class="side-list">
+             <a href="<?= HOSTNAME ?>dash/">
+                 <i class="fa-solid fa-chart-simple active"></i>Home</a>
+             <a href="<?= HOSTNAME ?>dash/requestlist.views.php">
+                 <i class="fa-solid fa-user"></i>Request List</a>
+             <a href="<?= HOSTNAME ?>dash/edit">
+                 <i class="fa-regular fa-user"></i>Settings</a>
+             <a href="<?= HOSTNAME ?>logout">
+                 <i class="fa-solid fa-arrow-right-from-bracket"></i>Log Out</a>
 
-        </div>
-        <div class="hamburger1">
-            <div class="bar1"></div>
-            <div class="bar1"></div>
-            <div class="bar1"></div>
-        </div>
-        <?php  } 
-                ?>  
-    </div>
-
- 
+         </div>
+         <div class="hamburger1">
+             <div class="bar1"></div>
+             <div class="bar1"></div>
+             <div class="bar1"></div>
+         </div>
+     <?php  }
+        ?>
+ </div>
