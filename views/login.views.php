c60898599b6b28b2412c876dbc417aa6466f2bc4
Subash1000 <be2020se744@gces.edu.np>
2023-07-18 15:40:32 +0545

    revoking previously made changes

--- views/login.views.php
+++ views/login.views.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" href="<?= HOSTNAME ?>/assets/css/style.css">
-</head>
-<body>
-    <div class="form">
-        <h1>Login</h1>
-        <form name="form" action="login" method="POST">
-            <div class="input-container">
-                <label for="email">Username:</label>
-                <input class="inputbox" type="text" name="username" id="email" placeholder="">
-            </div>
 
-            <div class="input-container">
-                <label for="password">Password:</label>
-                <input class="inputbox" type="password" name="password" id="password" placeholder="">
-            </div>
 
-            <input type="submit" name="login" value="Login">
+<form action="login" method="post">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <input type="submit" value="login" name="login">
 
-            <button type="button" onclick="location.href='register'">Create New Account</button>
-
-           
-        </form>
-    </div>
-</body>
-</html>
+</form>
--- views/register.views.php
+++ views/register.views.php
@@ -1,107 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Register</title>
-    <<link rel="stylesheet" href="<?= HOSTNAME ?>/assets/css/style.css">
-
-
-</head>
-<body>
-    <div class="register">
-        <form action="register" method="post" onsubmit="return validate(this)">
-            <h1>Register</h1>
-            <table>
-                <tr>
-                    <td>Firstname</td>
-                    <td>
-                        <input type="text" name="fname" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Middlename</td>
-                    <td>
-                        <input type="text" name="mname" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Lastname</td>
-                    <td>
-                        <input type="text" name="lname" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Blood Group</td>
-                    <td>
-                        <select name="bloodgroup">
-                            <option value="A+">A+</option>
-                            <option value="A-">A-</option>
-                            <option value="B+">B+</option>
-                            <option value="B-">B-</option>
-                            <option value="AB+">AB+</option>
-                            <option value="AB-">AB-</option>
-                            <option value="O+">O+</option>
-                            <option value="O-">O-</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Date of Birth</td>
-                    <td>
-                         <input type="date" name="dateofbirth" placeholder=" ">
-                   </td>
-                </tr>
-                 <tr>
-                    <td>Age</td>
-                    <td>
-                        <input type="number" name="age" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Email</td>
-                    <td>
-                        <input type="text" name="email" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td>
-                        <input type="password" name="password" placeholder=" ">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Username</td>
-                    <td>
-                        <input type="text" name="username" placeholder=" ">
-                    </td>
-                </tr>
-                 <tr>
-                     <td>Address</td>
-                    <td>
-                        <input class="inputbox" type="text" name="address" placeholder="">
-                    </td>
-                </tr>
-                <tr>
-                     <td>Phone number</td>
-                    <td>
-                        <input class="inputbox" type="text" name="phone_no" placeholder="">
-                    </td>
-                </tr>
-                 <tr>
-                     <td>Donor Availability</td>
-                    <td>
-                        <select class="inputbox" name="donor_availability">
-                            <option value="available">Available</option>
-                            <option value="unavailable">Unavailable</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" value="Register" name="register">
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </div>
-</body>
-</html>
+<!-- register -->
+<h1></h1>
+<form action="register" method="post">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <input type="text" name="email" placeholder="email">
+    <input type="text" name="phone" placeholder="phone">
+    <input type="text" name="address" placeholder="address">
+    <input type="text" name="bloodgroup" placeholder="bloodgroup">
+    <input type="submit" value="register" name="register">
+</form>
