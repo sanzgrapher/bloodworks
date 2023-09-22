fe73c626ac98ee53ebf637b5bc7115e2d600c3f2
Narayan Dhakal <narayandhakal443@gmail.com>
2023-09-22 14:55:53 +0545

    feat(function): smtp mail ffunction

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+include('smtp/PHPMailerAutoload.php');
 // $hostname = "http://localhost/bloodworks/";
 function show($stuff)
 {
@@ -35,6 +35,36 @@ function checkIflLoggedIn(){
     }
     else{
         return false;
+    }
+}
+
+function smtp_mailer($to, $subject, $msg)
+{
+    $mail = new PHPMailer();
+    $mail->SMTPDebug  = 0;
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587;
+    $mail->IsHTML(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->Username = "dbolar368@gmail.com";
+    $mail->Password = "dfypckleorqhrpxr";
+    $mail->SetFrom("dbolar368@gmail.com", "Bloodbank");
+    $mail->Subject = $subject;
+    $mail->Body = $msg;
+    $mail->AddAddress($to);
+    $mail->SMTPOptions = array('ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => false
+    ));
+    if (!$mail->Send()) {
+
+        die("Error");
+    } else {
+        return 'Sent';
     }
 }
 
