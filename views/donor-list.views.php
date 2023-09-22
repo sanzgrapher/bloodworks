1021a9bbc2fa2e7be2736940ddcb9665f0867b26
Narayan Dhakal <narayandhakal443@gmail.com>
2023-09-22 14:57:51 +0545

    refactor: test form for send mail function

--- views/donor-list.views.php
+++ views/donor-list.views.php
@@ -14,13 +14,13 @@ $sn = 0;
     <div class="table-section">
         <h1 class="table-sec-title fw-bold display-5">Available donors</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi harum accusantium ducimus vero ipsa dolore sint praesentium veritatis hic sapiente, nemo vitae perferendis quis inventore numquam quo totam reiciendis?</p>
-        <div style="overflow-x:auto;"> 
+        <div style="overflow-x:auto;">
 
 
             <table id="datatable" class="table table-hover table-striped table-bordered table-sm" cellspacing=" 0" width="100%">
                 <thead>
                     <tr>
-                       
+
                         <th>Name</th>
                         <th>Bloodgroup</th>
                         <th>Address</th>
@@ -33,14 +33,20 @@ $sn = 0;
                 } else {
                     foreach ($donors as $donor) { ?>
                         <tr>
-                            
+
                             <td><?= $donor->fname . " " . $donor->mname . " " . $donor->lname; ?></td>
                             <td><?= $donor->bloodgroup; ?></td>
                             <td><?= $donor->address; ?></td>
                             <!-- <td><?= isset($donor->phone_no) ? $donor->phone_no : 'N/A'; ?></td> -->
 
 
-                            <td><a class="link-info" href="#">Contact</a></td>
+                            <td>
+                                <form action="donorlist" method="POST">
+                                    <input class="submit button" type="submit" name="sendmail" value="Send For Approval">
+
+                                    <!-- <input value="sendemail" type="submit" class="btn btn-primary"> -->
+                                </form>
+                            </td>
                         </tr>
                 <?php
                     }
@@ -54,6 +60,7 @@ $sn = 0;
 
 
 </main>
+
 
 
 
