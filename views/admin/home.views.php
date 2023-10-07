5929c6b800e7aa1cb400c0a18575002daec66586
ajaayushdeku <be2020se698@gces.edu.np>
2023-10-07 13:47:53 +0545

    refinement on the admin data card

--- views/admin/home.views.php
+++ views/admin/home.views.php
@@ -11,26 +11,48 @@
          <?php include_once 'asidemenu.php'; ?>
 
 
-         <div class="in ">
-             <div class="">
-                 <div class="bank-card-list">
-                     <div class="bank-card">
-                        <i class="fa-solid fa-users-line"></i>
-                         <div class="card-data">
+        <div class="in ">
+            <div class="">
+                <div class="bank-card-list">
+                    <div class="bank-card">
+                        <!-- <i class="fa-solid fa-users-line"></i> -->
+                        <i class="fa-solid fa-user"></i>
+                        <div class="card-data">
                             <a href="#">Users</a>
                             <p><?= $totalusers ?></p>
-                         </div>
+                        </div>
                          
-                     </div>
-                     <div class="bank-card">
+                    </div>
+                    <div class="bank-card">
                         <i class="fa-solid fa-house-medical"></i>
-                         <div class="card-data">
-                         <a href="#">Blood Banks</a>
-                         <p><?= $totalbloodbanks ?></p>
-                         </div>
-                     </div>
-                 </div>
-             </div>
+                        <div class="card-data">
+                            <a href="#">Blood Banks</a>
+                            <p><?= $totalbloodbanks ?></p>
+                        </div>
+                    </div>
+                    <div class="bank-card">
+                        <i class="fa-solid fa-calendar-check"></i>
+                        <div class="card-data">
+                            <a href="#">Events</a>
+                            <p><?= $totalbloodbanks ?></p>
+                        </div>
+                    </div>
+                    <div class="bank-card">
+                        <i class="fa-solid fa-cubes-stacked"></i>
+                        <div class="card-data">
+                            <a href="#">Blood Stocks</a>
+                            <p><?= $totalbloodbanks ?></p>
+                        </div>
+                    </div>
+                    <div class="bank-card">
+                        <i class="fa-solid fa-users-line"></i>
+                        <div class="card-data">
+                            <a href="#">Event Participates</a>
+                            <p><?= $totalbloodbanks ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
 
