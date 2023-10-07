93f3e0681858156c185d2fc66ca558dd1b836118
Narayan Dhakal <narayandhakal443@gmail.com>
2023-10-07 20:13:35 +0545

    style: formatted

--- controllers/Event.php
+++ controllers/Event.php
@@ -8,7 +8,7 @@ class Event extends User
 
     public function index($a = "", $b = "", $c = "")
     {
-        $event_id=$a;
+        $event_id = $a;
 
         $uid = getLoggedinUser('userid');
 
@@ -16,37 +16,25 @@ class Event extends User
         $checkin = $this->isCheckIn($event_id, $uid);
 
         $userDetails = $this->getUserData(); // fom user class
-        // print avaiability
-       $u_d=$userDetails[0];
-
-      
-    //    show($u_d);
-
+          $u_d = $userDetails[0];
+          
         $data = [
             "userAvaiability" => $u_d->donor_availability ?? "",
             "isCheckIn" => $checkin,
             "eventData" => $eventDetails,
-
         ];
+
         if (isset($_POST['eventCheckIn-btn'])) {
-
-
-
-
-            $this->eventCheckIn($event_id, $uid);
+        $this->eventCheckIn($event_id, $uid);
         }
 
-
-
         $this->view("event", $data); // from controller class 
-
-
     }
+// --------------------------------------------------------------------------
     public function isCheckIn($event_id, $uid) // check is already turned in
-
     {
- 
-         
+
+
         $isCheckIn = new Model;
         $isCheckIn->table = "event_participants";
         $isCheckIn->order_column = "participation_id";
@@ -55,31 +43,23 @@ class Event extends User
             "event_id" => $event_id,
             "user_id" => $uid,
         ];
-        
+
 
 
         $isCheckIn = $isCheckIn->where($data);
-       
+
         if (empty($isCheckIn)) {
             return false; // not checked in
         } else {
             return true; // checked in
         }
-  
-   
-         
-      
-
     }
-    public function eventCheckIn($event_id, $uid){
-
-
-        
-
-
+// --------------------------------------------------------------------------
+    public function eventCheckIn($event_id, $uid)
+    {
         $eventCheckIn = new Model;
         $eventCheckIn->table = "event_participants";
-        
+
         $data = [
             "event_id" => $event_id,
             "user_id" => $uid,
@@ -91,13 +71,10 @@ class Event extends User
         } else {
             echo "user not registered";
         }
- 
-   
-
-
     }
-
-    public function eventDetails($event_id){
+// --------------------------------------------------------------------------
+    public function eventDetails($event_id)
+    {
         $eventDetails = new Model;
         $eventDetails->table = "event";
         $eventDetails->order_column = "event_id";
@@ -108,6 +85,4 @@ class Event extends User
         $eventDetails = $eventDetails->where($data);
         return $eventDetails;
     }
-    
-
 }
