ff55ae10b956bf1c3c02d425285a3b6d06f08d99
Narayan Dhakal <narayandhakal443@gmail.com>
2023-10-08 14:36:31 +0545

    style: format document

--- views/bbadmin/bloodstock.views.php
+++ views/bbadmin/bloodstock.views.php
@@ -7,7 +7,7 @@ include_once 'views/header.php';
 <main class="dash-container">
     <section class="dashboard_container">
         <?php include_once 'asidemenu.php'; ?>
-        <div class="info ">
+        <div class="info">
             <div class="container">
                 <div class="heading">
                     Blood Bank's Blood Stocks
@@ -83,38 +83,38 @@ include_once 'views/header.php';
                             <div class="fields">
                                 <div class="input_field">
                                     <label>A Positive</label>
-                                    <input type="text" name="a_pos" value="<?= $bloodstock->a_pos; ?> " placeholder="Enter the quantity of A positive bloodstocks" required>
+                                    <input type="number" name="a_pos" value="<?= $bloodstock->a_pos; ?>" placeholder="Enter the quantity of A positive bloodstocks" required>
                                 </div>
                                 <div class="input_field">
                                     <label>A Negative</label>
-                                    <input type="text" name="a_neg" value="<?= $bloodstock->a_neg; ?> " placeholder="Enter the quantity of A negative bloodstocks" required>
+                                    <input type="number" name="a_neg" value="<?= $bloodstock->a_neg; ?>" placeholder="Enter the quantity of A negative bloodstocks" required>
                                 </div>
 
                                 <div class="input_field">
                                     <label>B Positive</label>
-                                    <input type="text" name="b_pos" value="<?= $bloodstock->b_pos ?> " placeholder="Enter the quantity of B positive bloodstocks" required>
+                                    <input type="number" name="b_pos" value="<?= $bloodstock->b_pos ?>" placeholder="Enter the quantity of B positive bloodstocks" required>
                                 </div>
                                 <div class="input_field">
                                     <label>B Negative</label>
-                                    <input type="text" name="b_neg" value="<?= $bloodstock->b_neg ?> " placeholder="Enter the quantity of B negative bloodstocks" required>
+                                    <input type="number" name="b_neg" value="<?= $bloodstock->b_neg ?>" placeholder="Enter the quantity of B negative bloodstocks" required>
                                 </div>
 
                                 <div class="input_field">
                                     <label>AB Positive</label>
-                                    <input type="text" name="ab_pos" value="<?= $bloodstock->ab_pos ?> " placeholder="Enter the quantity of AB positive bloodstocks" required>
+                                    <input type="number" name="ab_pos" value="<?= $bloodstock->ab_pos ?>" placeholder="Enter the quantity of AB positive bloodstocks" required>
                                 </div>
                                 <div class="input_field">
                                     <label>AB Negative</label>
-                                    <input type="text" name="ab_neg" value="<?= $bloodstock->ab_neg ?> " placeholder="Enter the quantity of AB negative bloodstocks" required>
+                                    <input type="number" name="ab_neg" value="<?= $bloodstock->ab_neg ?>" placeholder="Enter the quantity of AB negative bloodstocks" required>
                                 </div>
 
                                 <div class="input_field">
                                     <label>O Positive</label>
-                                    <input type="text" name="o_pos" value="<?= $bloodstock->o_pos ?> " placeholder="Enter the quantity of O positive bloodstocks" required>
+                                    <input type="number" name="o_pos" value="<?= $bloodstock->o_pos ?>" placeholder="Enter the quantity of O positive bloodstocks" required>
                                 </div>
                                 <div class="input_field">
                                     <label>O Negative</label>
-                                    <input type="text" name="o_neg" value="<?= $bloodstock->o_neg ?> " placeholder="Enter the quantity of O negative bloodstocks" required>
+                                    <input type="number" name="o_neg" value="<?= $bloodstock->o_neg ?>" placeholder="Enter the quantity of O negative bloodstocks" required>
                                 </div>
                             </div>
                             <input class="submit button" type="submit" name="updatebloodstock" value="Update Details">
