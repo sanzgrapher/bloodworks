b41fda20fbf9655c98a7ceadcd4b11d2f8cbb692
Narayan Dhakal <narayandhakal443@gmail.com>
2023-10-08 14:36:06 +0545

    feat: admin home cards

--- views/admin/home.views.php
+++ views/admin/home.views.php
@@ -11,48 +11,61 @@
          <?php include_once 'asidemenu.php'; ?>
 
 
-        <div class="in ">
-            <div class="">
-                <div class="bank-card-list">
-                    <div class="bank-card">
-                        <!-- <i class="fa-solid fa-users-line"></i> -->
-                        <i class="fa-solid fa-user"></i>
-                        <div class="card-data">
-                            <a href="#">Users</a>
-                            <p><?= $totalusers ?></p>
-                        </div>
-                         
-                    </div>
-                    <div class="bank-card">
-                        <i class="fa-solid fa-house-medical"></i>
-                        <div class="card-data">
-                            <a href="#">Blood Banks</a>
-                            <p><?= $totalbloodbanks ?></p>
-                        </div>
-                    </div>
-                    <div class="bank-card">
-                        <i class="fa-solid fa-calendar-check"></i>
-                        <div class="card-data">
-                            <a href="#">Events</a>
-                            <p><?= $totalbloodbanks ?></p>
-                        </div>
-                    </div>
-                    <div class="bank-card">
-                        <i class="fa-solid fa-cubes-stacked"></i>
-                        <div class="card-data">
-                            <a href="#">Blood Stocks</a>
-                            <p><?= $totalbloodbanks ?></p>
-                        </div>
-                    </div>
-                    <div class="bank-card">
-                        <i class="fa-solid fa-users-line"></i>
-                        <div class="card-data">
-                            <a href="#">Event Participates</a>
-                            <p><?= $totalbloodbanks ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+         <div class="in ">
+             <div class="">
+                 <div class="bank-card-list">
+                     <div class="bank-card">
+                         <!-- <i class="fa-solid fa-users-line"></i> -->
+                         <i class="fa-solid fa-user"></i>
+                         <div class="card-data">
+                             <a href="#">Users</a>
+                             <p><?= $totalusers ?></p>
+                         </div>
+
+                     </div>
+                     <div class="bank-card">
+                         <!-- <i class="fa-solid fa-users-line"></i> -->
+                         <i class="fa-solid fa-user"></i>
+                         <div class="card-data">
+                             <a href="#">Donors</a>
+                             <p><?= $totalvusers ?></p>
+                         </div>
+
+                     </div>
+                     <div class="bank-card">
+                         <i class="fa-solid fa-house-medical"></i>
+                         <div class="card-data">
+                             <a href="#">Blood Banks</a>
+                             <p><?= $totalbloodbanks ?></p>
+                         </div>
+                     </div>
+                     <?php if (false) { ?>
+                         <div class="bank-card">
+                             <i class="fa-solid fa-calendar-check"></i>
+                             <div class="card-data">
+                                 <a href="#">Events</a>
+                                 <p><?= $totalbloodbanks ?></p>
+                             </div>
+                         </div>
+                         <div class="bank-card">
+                             <i class="fa-solid fa-cubes-stacked"></i>
+                             <div class="card-data">
+                                 <a href="#">Blood Stocks</a>
+                                 <p><?= $totalbloodbanks ?></p>
+                             </div>
+                         </div>
+                         <div class="bank-card">
+                             <i class="fa-solid fa-users-line"></i>
+                             <div class="card-data">
+                                 <a href="#">Event Participates</a>
+                                 <p><?= $totalbloodbanks ?></p>
+                             </div>
+                         </div>
+                     <?php } ?>
+
+
+                 </div>
+             </div>
 
 
 
